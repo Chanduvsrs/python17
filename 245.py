591674ab24d419c0b8c17540a352da2e5a546d8d
v chandu <administrator@MCA.COM>
2023-12-20 16:07:42 +0530

    first commit

---
+++ 245.py
@@ -0,0 +1,5 @@
+a=(input('enter age of a person :-'))
+if int(a)>==8):
+    print('eligible fo vote')
+else:
+    print('not eligible for vote')
---
+++ python/python.py1.py
@@ -0,0 +1,6 @@
+a=input('enter a character:-')
+if a in 'ABCDEFGHIJKLMNOPQRSTUYWXYZ'and a is 1>0:
+    print('it is upper case')
+else:
+    print('it is not upper case')
+    print('executed')
